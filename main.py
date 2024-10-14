29fb74fa4faf63b76e75b29d2224ca1be4778d50
Mert0nys <168013436+Mert0nys@users.noreply.github.com>
2024-10-14 18:38:05 +0400

    final2

--- main.py
+++ main.py
@@ -7,35 +7,31 @@ WEATHER_URL = 'http://api.openweathermap.org/data/2.5/weather'
 
 bot = telebot.TeleBot(API_TOKEN)
 
-@bot.message_handler(commands=['start'])
+bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Привет! Напиши название города:")
+    bot.reply_to(message, "Привет! Напиши название города, чтобы узнать текущую погоду.")
 
 @bot.message_handler(func=lambda message: True)
 def get_weather(message):
     city = message.text
-    params = {
-        'q': city,
-        'appid': WEATHER_API_KEY,
-        'units': 'metric'  
-    }
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={WEATHER_API_KEY}&units=metric&lang=ru"
     
-    response = requests.get(WEATHER_URL, params=params)
+    response = requests.get(url)
     
     if response.status_code == 200:
         data = response.json()
-        temp = data['main']['temp']
+        temperature = data['main']['temp']
         humidity = data['main']['humidity']
         weather_description = data['weather'][0]['description']
         
-        weather_info = (f"Погода в {city}:\n"
-                        f"Температура: {temp} С\n"
-                        f"Влажность: {humidity} %\n"
-                        f"Описание: {weather_description.capitalize()}.")
-        
+        weather_info = (
+            f"Погода в городе {city}:\n"
+            f"Температура: {temperature}°C\n"
+            f"Влажность: {humidity}%\n"
+            f"Описание: {weather_description.capitalize()}\n"
+        )
         bot.reply_to(message, weather_info)
     else:
-        bot.reply_to(message, "Город не найден. Попробуйте еще раз.")
+        bot.reply_to(message, "Не удалось получить данные о погоде. Проверьте название города.")
 
-if __name__ == '__main__':
-    bot.polling(none_stop=True)
+bot.polling()
